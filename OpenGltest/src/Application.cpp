08d1668bc80bb6bf4cd2013abd77e4b7d99d5e50
JD <jd@jdnet.de>
2021-09-11 11:42:05 +0200

    implements index buffer to draw square

--- OpenGltest/src/Application.cpp
+++ OpenGltest/src/Application.cpp
@@ -115,21 +115,33 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     // float triangle positions
-    float vertex[6] = {
-        -0.5,-0.5,
-         0.0, 0.5,
-         0.5,-0.5,
+    float vertex[] = {
+        -0.5f,-0.5f,
+         0.5f,-0.5f,
+         0.5f, 0.5f,
+        -0.5f, 0.5f,
+    };
+
+    unsigned int indices[] = {
+        0,1,2,
+        2,3,0
     };
 
     // generate buffer ID
     unsigned int buffer;
     glGenBuffers(1, &buffer);
-    // bind the buffer
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertex, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(indices)*2*sizeof(float), vertex, GL_STATIC_DRAW);
+
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
 
     ShaderProgramSource shaderProgramSource = ParseShader("res/shaders/Basic.shader");
     std::cout << shaderProgramSource.VertexSource << std::endl;
@@ -147,7 +159,8 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
